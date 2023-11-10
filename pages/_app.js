c376b671b0bc6429d08ca55b6b47b35f0d4d9612
Marko Petrovic <markopetrovic753@gmail.com>
2023-11-10 23:08:05 +0100

    Dodata logika za navigacioni meni i zapocet dizajn

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import { Helmet } from "react-helmet";
 
 //INTERNAL IMPORT
 import { NavBar } from '@/components/component';
@@ -6,6 +7,11 @@ import { NavBar } from '@/components/component';
 
 const MyApp = ({ Component, pageProps }) => (
     <div>
+        <Helmet>
+                <meta charSet="utf-8" />
+                <title>React app</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        </Helmet>
         <NavBar />
         <Component {...pageProps} />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>React app</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        </Helmet>
+    </div>
   )
 }
 
