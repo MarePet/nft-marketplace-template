6a0c5a80746fb6f53447d8774e4cba07c3b6b469
Marko Petrovic <markopetrovic753@gmail.com>
2024-01-20 20:24:54 +0100

    Popravke CSS i responzivnosti

---
+++ components/Filter/Filter.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import {
+    FaFilter,
+    FaAngleDown,
+    FaAngleUp,
+    FaWallet,
+    FaMusic,
+    FaVideo,
+    FaImage,
+    FaUserAlt,
+    FaImages
+} from 'react-icons/fa'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { MdVerified } from 'react-icons/md'
+import { TiTick } from 'react-icons/ti'
+
+//INTERNAL IMPORT
+import Style from './Filter.module.css'
+
+const Filter = () => {
+    const [filter, setFilter] = useState(true);
+    const [image, setImage] = useState(true);
+    const [video, setVideo] = useState(true);
+    const [music, setMusic] = useState(true);
+
+    //FUNCTIONS
+    const openFilter = () => {
+        if(!filter){
+            setFilter(true)
+        }else{
+            setFilter(false)
+        }
+    }
+
+    const openImage = () =>{
+        if(!image){
+            setImage(true);
+        }
+        else{
+            setImage(false);
+        }
+    }
+
+    const openVideo = () => {
+        if(!video){
+            setVideo(true);
+        }
+        else{
+            setVideo(false);
+        }
+    }
+
+    const openMusic = () => {
+        if(!music){
+            setMusic(true);
+        }
+        else{
+            setMusic(false);
+        }
+    }
+
+
+    return (
+        <div className={Style.Filter}>
+            <div className={Style.Filter_Box}>
+                <div className={Style.Filter_Box_Left}>
+                    <button onClick={() => { }}>NFTs</button>
+                    <button onClick={() => { }}>Art</button>
+                    <button onClick={() => { }}>Music</button>
+                    <button onClick={() => { }}>Sport</button>
+                    <button onClick={() => { }}>Photography</button>
+                </div>
+                <div className={Style.Filter_Box_Right}>
+                    <div className={Style.Filter_Box_Right_Box}
+                        onClick={() => openFilter()}>
+                        <FaFilter />
+                        <span>Filter</span> {filter ? <FaAngleDown /> : <FaAngleUp />}
+                    </div>
+                </div>
+            </div>
+            {
+                filter && (
+                    <div className={Style.Filter_Box_Items}>
+                        <div className={Style.Filter_Box_Items_Box}>
+                            <div className={Style.Filter_Box_Items_Box_Item}>
+                                <FaWallet /> <span>0.01 ETH - 10 ETH</span>
+                                <AiFillCloseCircle />
+                            </div>
+                        </div>
+                        <div className={Style.Filter_Box_Items_Box}>
+                            <div className={Style.Filter_Box_Items_Box_Item_Trans}
+                                onClick={() => openImage()}>
+                                    <FaImages/> <small>Images</small>
+                                    {image ? <AiFillCloseCircle/> : <TiTick/>}
+                            </div>
+                        </div>
+                        <div className={Style.Filter_Box_Items_Box}>
+                            <div className={Style.Filter_Box_Items_Box_Item_Trans}
+                            onClick={()=>openVideo()}>
+                                <FaVideo/> <small>Video</small>
+                                {
+                                    video ? <AiFillCloseCircle/> : <TiTick/>
+                                }
+                            </div>
+                        </div>
+                        <div className={Style.Filter_Box_Items_Box}>
+                            <div className={Style.Filter_Box_Items_Box_Item_Trans}
+                            onClick={()=>openMusic()}>
+                                <FaMusic/><small>Music</small>
+                                {
+                                    music ? <AiFillCloseCircle/> : <TiTick/>
+                                }
+                            </div>
+                        </div>
+                        <div className={Style.Filter_Box_Items_Box}>
+                            <div className={Style.Filter_Box_Items_Box_Item}>
+                                <FaUserAlt/> <span>Verified</span>
+                                <MdVerified/>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default Filter
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //INTERNAL IMPORT
 import Style from '../styles/index.module.css';
-import { Hero , Service, BigNFTSlider, Subscribe, Title, Category} from '@/components/component';
+import { Hero , Service, BigNFTSlider, Subscribe, Title, Category,Filter} from '@/components/component';
 
 
 const Home = () => {
@@ -11,6 +11,9 @@ const Home = () => {
       <Hero/>
       <Service/>
       <BigNFTSlider/>
+      <Title heading="Featured NFTs" 
+      paragraph="Discover the most outsdanding NFTs."/>
+      <Filter/>
       <Title heading="Browse by category" 
       paragraph="Explore NFTs by the featured categories. "/>
       <Category/>
